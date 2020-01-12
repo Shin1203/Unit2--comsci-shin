72c9710c4a675b26754c7de6172ce792bf36f0b9
Shin M <54606968+Shin1203@users.noreply.github.com>
2020-01-12 17:12:29 +0900

    Update programmingpractiseloopsC.C

--- programmingpractiseloopsC.C
+++ programmingpractiseloopsC.C
@@ -1,5 +1,5 @@
+TASK 1
 #include <stdio.h>
-
 int main(void) {
   int number=0;
   int sum=0;
@@ -13,3 +13,22 @@ int main(void) {
   printf("Your sum is %d", sum);
   return 0;
 }
+
+
+TASK 2
+ #include <stdio.h>
+int main(void) {
+  int terms=0;
+  int cube=0;
+  int count=0;
+  printf("Enter number of terms to cube\n");
+  scanf("%d", &terms);
+  
+  for (int i=1; i<=terms; i++){
+    cube=(i*i*i);
+    printf(" %d ", cube);
+  }
+  return 0;
+}
+
+TASK 3
