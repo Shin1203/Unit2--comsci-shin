db28b8e3b91c1d4b4c2a8e9b415bca58dbbf7b2d
Shin M <54606968+Shin1203@users.noreply.github.com>
2020-01-12 18:02:48 +0900

    Update programmingpractiseloopsC.C

--- programmingpractiseloopsC.C
+++ programmingpractiseloopsC.C
@@ -65,3 +65,22 @@ sum=(sum+(1/i));
   printf("The sum of the harmonic series of %d is %f",number,sum);
   return 0;
 }
+
+TASK 5
+  #include <stdio.h>
+
+int main(void) {
+  int terms=0;
+  int sum=0;
+  int number=1;
+ 
+  printf("This series adds a number to the sum that increments in one digit with the number 1, going 1,11,111,1111 etc. Enter a number of terms for the series\n");
+ scanf("%d", &terms);
+for (int i=1;terms>=i; i++){
+     sum=(sum+number);
+     number=((number*10)+1);
+}
+ printf("Your sum is %d", sum);
+ 
+ return 0;
+}
